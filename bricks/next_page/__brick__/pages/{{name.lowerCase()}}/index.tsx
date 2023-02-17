8240cdcbefc567c0d751e28b343014d32d2be2c0
Filip Ljubic <f.ljubic7@hotmail.com>
2023-02-17 10:26:58 +0100

    fix import

--- bricks/next_page/__brick__/pages/{{name.lowerCase()}}/index.tsx
+++ bricks/next_page/__brick__/pages/{{name.lowerCase()}}/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import {{name}} from "../../components/{{name}}/{{name}}";
+import {{name}} from "@components/{{name.lowerCase()}}/{{name}}";
 
 const {{name}}Page: NextPage = (props) => {
   return (
